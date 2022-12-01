2adfedc3b22d87dc294eaed516ff954c0c1d6023
Kevin Borduas <kborduas@gmail.com>
2022-11-30 22:21:43 -0500

    Prep for Day 01

---
+++ Aoc Utilities/DataLoader.cs
@@ -0,0 +1,15 @@
+﻿namespace Utilities;
+
+public class DataLoader
+{
+    public static int[] GetIntDataFromFile(bool isTest = false)
+    {
+        return GetStringDataFromFile(isTest).Select(line => Int32.Parse(line)).ToArray();
+    }
+
+    public static string[] GetStringDataFromFile(bool isTest = false)
+    {
+        var fileName = isTest ? "Test" : "Puzzle";
+        return File.ReadAllLines($@"..\..\..\Data\{fileName}Input.txt");
+    }
+}
