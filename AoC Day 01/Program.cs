1b7c24aea2db2f9d64a3b0e40c86974ce3961771
Kevin Borduas <kborduas@gmail.com>
2022-12-01 00:13:21 -0500

    AoC Day 01 Solutions 1 and 2

--- AoC Day 01/Program.cs
+++ AoC Day 01/Program.cs
@@ -5,24 +5,52 @@ SolvePuzzleTwo();
 
 void SolvePuzzleOne()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
+
+    var currentCalories = 0;
+    var elvesList = new List<int>();
 
     for (var i = 0; i < data.Length; i++)
     {
-
+        if (data[i] == String.Empty)
+        {
+            elvesList.Add(currentCalories);
+            currentCalories = 0;
+        }
+        else
+        {
+            currentCalories += Int32.Parse(data[i]);
+        }
     }
 
-    Console.WriteLine($"Réponse 1 : ");
+    elvesList.Add(currentCalories);
+
+    Console.WriteLine($"Réponse 1 : " + elvesList.OrderByDescending(x => x).First());
 }
 
 void SolvePuzzleTwo()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
+
+    var currentCalories = 0;
+    var elvesList = new List<int>();
 
     for (var i = 0; i < data.Length; i++)
     {
-
+        if (data[i] == String.Empty)
+        {
+            elvesList.Add(currentCalories);
+            currentCalories = 0;
+        }
+        else
+        {
+            currentCalories += Int32.Parse(data[i]);
+        }
     }
 
-    Console.WriteLine($"Réponse 2 : ");
+    elvesList.Add(currentCalories);
+
+    var sortedElves = elvesList.OrderByDescending(x => x).ToList();
+
+    Console.WriteLine($"Réponse 2 : " + (sortedElves[0] + sortedElves[1] + sortedElves[2]));
 }
