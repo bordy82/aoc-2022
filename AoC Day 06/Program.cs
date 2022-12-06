1fa7dcbf9fe611324bfd03cd67ea97ea6130839c
Kevin Borduas <kborduas@gmail.com>
2022-12-06 09:59:58 -0500

    AoC Day 06 Solutions 1 and 2

--- AoC Day 06/Program.cs
+++ AoC Day 06/Program.cs
@@ -5,24 +5,56 @@ SolvePuzzleTwo();
 
 void SolvePuzzleOne()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile().First();
 
-    for (var i = 0; i < data.Length; i++)
+    var index = 0;
+    var signal = data.Substring(0, 4);
+
+    for(var i = 4; i < data.Length; i++)
     {
+        if (ValidateSignal(signal))
+        {
+            index = i;
+            break;
+        }
 
+        signal = (signal + data[i]).Substring(1, 4);
     }
 
-    Console.WriteLine($"Réponse 1 : ");
+    Console.WriteLine($"Réponse 1 : " + index);
 }
 
 void SolvePuzzleTwo()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile().First();
 
-    for (var i = 0; i < data.Length; i++)
+    var index = 0;
+    var signal = data.Substring(0, 14);
+
+    for (var i = 14; i < data.Length; i++)
     {
+        if (ValidateSignal(signal))
+        {
+            index = i;
+            break;
+        }
 
+        signal = (signal + data[i]).Substring(1, 14);
     }
 
-    Console.WriteLine($"Réponse 2 : ");
+    Console.WriteLine($"Réponse 2 : " + index);
+}
+
+bool ValidateSignal(string signal)
+{
+    var different = true;
+    foreach (var c in signal)
+    {
+        if (signal.Count(x => x == c) > 1)
+        {
+            different = false;
+        }
+    }
+
+    return different;
 }
