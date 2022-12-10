7de9833e781772a451a3e007c1561702b1e5f35a
Kevin Borduas <kborduas@gmail.com>
2022-12-09 21:34:09 -0500

    Prep for next days

---
+++ AoC Day 12/Program.cs
@@ -0,0 +1,28 @@
+﻿using Utilities;
+
+SolvePuzzleOne();
+SolvePuzzleTwo();
+
+void SolvePuzzleOne()
+{
+    var data = DataLoader.GetStringDataFromFile(true);
+
+    for (var i = 0; i < data.Length; i++)
+    {
+
+    }
+
+    Console.WriteLine($"Réponse 1 : ");
+}
+
+void SolvePuzzleTwo()
+{
+    var data = DataLoader.GetStringDataFromFile(true);
+
+    for (var i = 0; i < data.Length; i++)
+    {
+
+    }
+
+    Console.WriteLine($"Réponse 2 : ");
+}
