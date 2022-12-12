9fa726b315252f39b793a0e0588f42ad0c6d8464
Kevin Borduas <kborduas@gmail.com>
2022-12-11 21:19:06 -0500

    AoC Day 11 Solutions 1 and 2

---
+++ AoC Day 11/Monkey.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_Day_11
+{
+    internal class Monkey
+    {
+        public int Id { get; private set; }
+
+        public List<BigInteger> StartingItems { get; private set; }
+
+        public Operation Operation { get; set; }
+
+        public int OperationValue { get; set; }
+
+        public int TestDivisible { get; set; }
+
+        public int TestTrue { get; set; }
+
+        public int TestFalse { get; set; }
+
+        public long InspectCount { get; private set; }
+
+        public Monkey(int id)
+        {
+            Id = id;
+            this.InspectCount = 0;
+            this.StartingItems = new List<BigInteger>();
+        }
+
+        public BigInteger GetNewWorryLevel(BigInteger worryLevel)
+        {
+            this.InspectCount++;
+
+            var opsValue = this.OperationValue == -1 ? worryLevel : this.OperationValue;
+
+            if (this.Operation == Operation.Multiply)
+            {    
+                return worryLevel * opsValue;
+            }
+            else
+            {
+                return worryLevel + opsValue;
+            }
+        }
+
+        public BigInteger Test(BigInteger worryLevel)
+        {
+            if (worryLevel % this.TestDivisible == 0)
+            {
+                return this.TestTrue;
+            }
+            else
+            {
+                return this.TestFalse;
+            }
+        }
+    }
+}
+
+enum Operation
+{
+    Add,
+    Multiply
+}
--- AoC Day 11/Program.cs
+++ AoC Day 11/Program.cs
@@ -1,28 +1,121 @@
-﻿using Utilities;
+﻿using AoC_Day_11;
+using System.Numerics;
+using Utilities;
 
 SolvePuzzleOne();
 SolvePuzzleTwo();
 
 void SolvePuzzleOne()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
 
-    for (var i = 0; i < data.Length; i++)
+    var monkeys = GetMonkeys(data);
+
+    for (var i = 0; i < 20; i++)
     {
+        foreach (var monkey in monkeys)
+        {
+            foreach (var item in monkey.StartingItems)
+            {
+                var newWorryLevel = monkey.GetNewWorryLevel(item);
+                var boredWorryLevel = newWorryLevel / 3l;
+                var monkeyToThrowTo = monkey.Test(boredWorryLevel);
 
+                monkeys.First(x => x.Id == monkeyToThrowTo).StartingItems.Add(boredWorryLevel);
+            }
+
+            monkey.StartingItems.Clear();
+        }
     }
 
-    Console.WriteLine($"Réponse 1 : ");
+    var orderedMonkeys = monkeys.OrderByDescending(x => x.InspectCount).ToList();
+
+    Console.WriteLine($"Réponse 1 : " + (orderedMonkeys[0].InspectCount * orderedMonkeys[1].InspectCount));
 }
 
 void SolvePuzzleTwo()
 {
-    var data = DataLoader.GetStringDataFromFile(true);
+    var data = DataLoader.GetStringDataFromFile();
 
-    for (var i = 0; i < data.Length; i++)
+    var monkeys = GetMonkeys(data);
+        
+    var commonDenominator = 1;
+    foreach (var monkey in monkeys)
+        commonDenominator *= monkey.TestDivisible;
+
+    for (var i = 0; i < 10000; i++)
     {
+        foreach (var monkey in monkeys)
+        {
+            foreach (var item in monkey.StartingItems)
+            {
+                var newWorryLevel = monkey.GetNewWorryLevel(item);
+                var boredWorryLevel = newWorryLevel % commonDenominator;
+                var monkeyToThrowTo = monkey.Test(boredWorryLevel);
 
+                monkeys.First(x => x.Id == monkeyToThrowTo).StartingItems.Add(boredWorryLevel);
+            }
+
+            monkey.StartingItems.Clear();
+        }
     }
 
-    Console.WriteLine($"Réponse 2 : ");
+    var orderedMonkeys = monkeys.OrderByDescending(x => x.InspectCount).ToList();
+
+    Console.WriteLine($"Réponse 1 : " + (orderedMonkeys[0].InspectCount * orderedMonkeys[1].InspectCount));
+}
+
+List<Monkey> GetMonkeys(string[] data)
+{
+    var monkeys = new List<Monkey>();
+
+    Monkey currentMonkey = null;
+    for (var i = 0; i < data.Length; i++)
+    {
+        if (data[i].StartsWith("Monkey"))
+        {
+            currentMonkey = new Monkey(Int32.Parse(data[i].Substring(7, 1)));
+        }
+        else if (data[i].Contains("Starting items"))
+        {
+            currentMonkey.StartingItems.AddRange(data[i].Split("items: ")[1].Split(", ").Select(x => BigInteger.Parse(x)));
+        }
+        else if (data[i].Contains("Operation:"))
+        {
+            if (data[i].Contains("*"))
+            {
+                currentMonkey.Operation = Operation.Multiply;
+
+                var opsValue = data[i].Split("* ")[1];
+                currentMonkey.OperationValue = opsValue == "old" ? -1 : Int32.Parse(opsValue);
+            }
+            else
+            {
+                currentMonkey.Operation = Operation.Add;
+
+                var opsValue = data[i].Split("+ ")[1];
+                currentMonkey.OperationValue = opsValue == "old" ? -1 : Int32.Parse(opsValue);
+            }
+        }
+        else if (data[i].Contains("Test:"))
+        {
+            currentMonkey.TestDivisible = Int32.Parse(data[i].Split("by ")[1]);
+        }
+        else if (data[i].Contains("If true:"))
+        {
+            currentMonkey.TestTrue = Int32.Parse(data[i].Split("monkey ")[1]);
+        }
+        else if (data[i].Contains("If false:"))
+        {
+            currentMonkey.TestFalse = Int32.Parse(data[i].Split("monkey ")[1]);
+        }
+        else
+        {
+            monkeys.Add(currentMonkey);
+        }
+    }
+
+    monkeys.Add(currentMonkey);
+
+    return monkeys;
 }
